82a582fae026e876cbf1b19ec9c01c5b8e8aed64
yanic <yanic@YANIC>
2025-04-10 14:58:42 +0200

    Add project files.

---
+++ everave.server/Components/Controllers/ImageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace everave.server.Components.Controllers
+{
+    [Route("upload/[controller]")]
+    [ApiController]
+    public class ImageController(IWebHostEnvironment env) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile upload)
+        {
+            if (upload == null || upload.Length == 0)
+                return BadRequest("Upload failed");
+
+            var uploadsFolder = Path.Combine(env.WebRootPath, "uploads");
+            if (!Directory.Exists(uploadsFolder))
+                Directory.CreateDirectory(uploadsFolder);
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension(upload.FileName);
+            var filePath = Path.Combine(uploadsFolder, fileName);
+
+            await using (var stream = System.IO.File.Create(filePath))
+            {
+                await upload.CopyToAsync(stream);
+            }
+
+            var url = $"{Request.Scheme}://{Request.Host}/uploads/{fileName}";
+
+            return Ok(new { url });
+        }
+    }
+}
---
+++ everave.server/JSLib/src/index.js
@@ -0,0 +1,5 @@
+﻿import { initializeCKEditor } from './ckeditor_lib';
+
+export function InitializeEditor(editorId, dotNetHelper) {
+    initializeCKEditor(editorId, dotNetHelper);
+}
