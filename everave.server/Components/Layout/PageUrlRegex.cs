6189a11b96bad773a52f4a7d6ea37ff7b1f2cbee
Yanic Högger <yanic.hoegger@gmx.ch>
2025-04-18 14:32:02 +0200

    Added Navigation

---
+++ everave.server/Components/Layout/PageUrlRegex.cs
@@ -0,0 +1,33 @@
+﻿namespace everave.server.Components.Layout;
+
+public static class PageUrlRegex
+{
+    // AddTopic.razor
+    public const string AddTopicPattern = @"^/forum/(?<ForumId>[^/]+)/add-topic$";
+    public const string AddTopicForumIdGroup = "ForumId";
+
+    // Admin.razor
+    public const string AdminPattern = @"^/admin$";
+
+    // EditUser.razor
+    public const string EditUserPattern = @"^/admin/edit-user/(?<UserId>[^/]+)$";
+    public const string EditUserUserIdGroup = "UserId";
+
+    // ForumOverview.razor
+    public const string ForumOverviewPattern = @"^/forums$";
+
+    // ForumTopics.razor
+    public const string ForumTopicsPattern = @"^/forum/(?<Id>[^/]+)$";
+    public const string ForumTopicsIdGroup = "Id";
+
+    // Register.razor
+    public const string RegisterPattern = @"^/register$";
+
+    // TopicPage.razor
+    public const string TopicPagePattern = @"^/topic/(?<Id>[^/]+)$";
+    public const string TopicPageIdGroup = "Id";
+
+    // UserDetails.razor
+    public const string UserDetailsPattern = @"^/user-details/(?<UserId>[^/]+)$";
+    public const string UserDetailsUserIdGroup = "UserId";
+}
