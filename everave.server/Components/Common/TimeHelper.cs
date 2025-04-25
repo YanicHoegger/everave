f896ea236df29faccd66848377f8025ac5a0a84a
Yanic Högger <yanic.hoegger@gmx.ch>
2025-04-25 12:14:53 +0200

    - Added eve&rave color to css - Made time right

---
+++ everave.server/Components/Common/TimeHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace everave.server.Components.Common
+{
+    public static class TimeHelper
+    {
+        private static readonly TimeZoneInfo EuropeTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Europe/Zurich");
+
+        public static string PrintCET(this DateTime dateTime)
+        {
+            var localizedTime = TimeZoneInfo.ConvertTimeFromUtc(dateTime.ToUniversalTime(), EuropeTimeZone);
+            return localizedTime.ToString("g", new CultureInfo("de-CH"));
+        }
+    }
+}
