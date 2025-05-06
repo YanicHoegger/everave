44655b30b78d531295e2ee6099f5b23778fccda4
Yanic Högger <yanic.hoegger@gmx.ch>
2025-05-06 17:21:13 +0200

    Improved Account component

---
+++ everave.server/Components/Common/ProfilePicture.cs
@@ -0,0 +1,14 @@
+﻿using everave.server.UserManagement;
+
+namespace everave.server.Components.Common
+{
+    public static class ProfilePicture
+    {
+        public static string GetProfilePictureUrl(this ApplicationUser? user)
+        {
+            return string.IsNullOrEmpty(user?.ProfilePictureUrl)
+                ? "/no_avatar.gif"
+                : user.ProfilePictureUrl;
+        }
+    }
+}
--- everave.server/wwwroot/js/site.js
+++ everave.server/wwwroot/js/site.js
@@ -13,6 +13,12 @@ window.registerOutsideClickHandler = (dotNetHelper, popupSelector, buttonSelecto
         // Check if the click is outside the popup and button
         if (popup && button && !popup.contains(event.target) && !button.contains(event.target)) {
             dotNetHelper.invokeMethodAsync('ClosePopup');
+        } else if (popup && popup.contains(event.target)) {
+            // Check if the click is on a button or link inside the popup
+            const clickedElement = event.target;
+            if (clickedElement.tagName === 'BUTTON' || clickedElement.tagName === 'A' || clickedElement.closest('button') || clickedElement.closest('a')) {
+                dotNetHelper.invokeMethodAsync('ClosePopup');
+            }
         }
     });
 };
